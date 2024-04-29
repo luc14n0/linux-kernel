146035da64c2423ce9496a176b0cf6b2eea5e29f
Luciano Santos <luc14n0@opensuse.org>
2024-04-29 01:47:27 -0300

    Add openSUSE RPM build resources

---
+++ openSUSE/config.sh
@@ -0,0 +1,20 @@
+# The version of the main tarball to use
+SRCVERSION=6.1.83
+# variant of the kernel-source package, either empty or "-rt"
+VARIANT=-vanilla
+# Set to 1 to use the variant kernel for kernel-obs-build
+OBS_BUILD_VARIANT=1
+# enable kernel module compression
+COMPRESS_MODULES="xz"
+# Compile binary devicetrees on master and stable branches.
+BUILD_DTBS="Yes"
+BUILD_HTML=Yes
+BUILD_PDF=Yes
+# Generate a _multibuild file
+MULTIBUILD="Yes"
+# buildservice projects to build the kernel against
+OBS_PROJECT=openSUSE:Factory
+OBS_PROJECT_ARM=openSUSE:Factory:ARM
+OBS_PROJECT_PPC=openSUSE:Factory:PowerPC
+IBS_PROJECT=SUSE:Factory:Head
+IBS_PROJECT_ARM=Devel:ARM:Factory
